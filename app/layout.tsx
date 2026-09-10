eb55541857690593556113d63b8997fb0c462e45
Adamcod45 <obatayoadam1@gmail.com>
2026-09-10 16:16:43 +0100

    Update Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
+import { Toaster } from "react-hot-toast";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +12,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Adam Portfolio",
   description: "This is my portfolio",
+   icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -23,7 +27,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        {/* <StarsCanvas /> */}
+          <Toaster position="top-right" />
+        <StarsCanvas />
         <Navbar />
         {children}
         <Footer />
